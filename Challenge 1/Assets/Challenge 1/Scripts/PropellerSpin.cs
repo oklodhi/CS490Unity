c2d171232c4ea905681c3a162052302df3ca5b02
Omer Khan <okhanlodhi@gmail.com>
2021-03-22 20:17:51 -0500

    Commit Week 1 assignments

---
+++ Challenge 1/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    private float speed = 1000.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // spin the propeller
+        transform.Rotate(Vector3.forward * Time.deltaTime * speed); //rotates 50 degrees per second around z axis
+    }
+}
