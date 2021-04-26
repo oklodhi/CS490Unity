b34fbfc63dd28be61d38b45c4cb72dc49d6d40e3
Omer Khan <okhanlodhi@gmail.com>
2021-04-25 22:10:45 -0500

    Adding prototype 5, challenge 5, particle work, skills 4

---
+++ Challenge 5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagerX : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+
+    public TextMeshProUGUI timerText;
+
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    public Button restartButton; 
+
+    public List<GameObject> targetPrefabs;
+
+    public float currentTime;
+    public float startingTime=60f;
+
+    private int score;
+    private float spawnRate = 1.5f;
+    public bool isGameActive;
+
+    private float spaceBetweenSquares = 2.5f; 
+    private float minValueX = -3.75f;
+    private float minValueY = -3.75f; 
+    
+    public void StartGame(int difficulty)
+    {
+        currentTime = startingTime;
+        spawnRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.SetActive(false);
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text="Time: " + (Mathf.Round(currentTime));
+
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            if (currentTime<= 0)
+            {
+                GameOver();
+            }
+
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+
+            if (isGameActive)
+            {
+                Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation);
+            }
+            
+        }
+    }
+
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
+        float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
+        return spawnPosition;
+
+    }
+
+    int RandomSquareIndex()
+    {
+        return Random.Range(0, 4);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "score: "+ score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    private Rigidbody rigidbody;
+
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    private GameManager gameManager;
+
+    public int pointValue;
+
+    public ParticleSystem particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        rigidbody.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(particleSystem, transform.position, particleSystem.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+}
